9967668bcaa15570fdcdfd4499fb1c71949b7924
Mohamed Hossam <81392900+mohamedhoss123@users.noreply.github.com>
2024-06-18 08:06:21 +0300

    init files

---
+++ src/main.ts
@@ -0,0 +1,45 @@
+
+
+import session from 'express-session';
+
+
+class DrizzleSessionStore extends session.Store {
+  constructor(private readonly db: any, private readonly sessionTable: any) {
+    super();
+  }
+
+  async get(sid: string, callback: (err: any, session?: session.SessionData | null) => void) {
+    try {
+      const result = await this.db.select().from(this.sessionTable).where({ sid }).limit(1);
+      if (result.length) {
+        const sess = result[0];
+        callback(null, sess.session);
+      } else {
+        callback(null, null);
+      }
+    } catch (err) {
+      callback(err);
+    }
+  }
+
+  async set(sid: string, session: session.SessionData, callback?: (err?: any) => void) {
+    try {
+      const expire = new Date(session.cookie.expires as any);
+      await this.db.insertInto(this.sessionTable).values({ sid, session, expire }).onConflict().merge().execute();
+      callback?.();
+    } catch (err) {
+      callback?.(err);
+    }
+  }
+
+  async destroy(sid: string, callback?: (err?: any) => void) {
+    try {
+      await this.db.deleteFrom(this.sessionTable).where({ sid }).execute();
+      callback?.();
+    } catch (err) {
+      callback?.(err);
+    }
+  }
+}
+
+export default DrizzleSessionStore;
